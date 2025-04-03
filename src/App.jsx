c6cd69545716e2ae92df496c6b094238189b5213
Hmute <heraldoabreusound@gmail.com>
2025-04-03 15:23:04 -0700

    h

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,4 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import NavBar from './components/NavBar';
-import Home from './pages/Home';
-import AddStudent from './pages/AddStudent';
-import EditStudent from './pages/EditStudent';
 import './App.css';
 
 function App() {
